7411549b532f810fe0fbfe27d449a704fadcbab1
weber <weber0216@gmail.com>
2022-08-01 22:11:58 +0800

    update basic motion without look around

---
+++ Assets/Scripts/motion.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Com.Kawaiisun.SimpleHostile{
+    public class motion : MonoBehaviour
+    {
+        
+        public float speed;
+        private Rigidbody body;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            Camera.main.enabled = false;
+            body = GetComponent<Rigidbody>();
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            float t_hmoved = Input.GetAxis("Horizontal");
+            float t_vmoved = Input.GetAxis("Vertical");
+
+            Vector3 t_direction = new Vector3(t_hmoved, 0, t_vmoved);
+            t_direction.Normalize();
+
+            body.velocity = transform.TransformDirection(t_direction)*speed*Time.deltaTime;
+
+
+
+
+        }
+    }
+}
